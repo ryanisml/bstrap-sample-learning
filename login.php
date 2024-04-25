82df8703b3087931516afec7f72d6cd8d42cafde
ryanisml <ryanisml@gmail.com>
2024-04-25 09:06:28 +0800

    Add database authentication to login.php

--- login.php
+++ login.php
@@ -1,13 +1,35 @@
 <!-- Script php post / get -->
 <?php
+include 'database.php';
+    session_start();
+    if(isset($_SESSION['username'])){
+        header('Location: index.php');
+    }
     if(isset($_POST['username']) && isset($_POST['password'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
-  
-        if($username == "admin" && $password == "admin"){
-            echo "<h2>Login Success</h2>";
-            header('Location: index.php');
-        }else{
+
+        $sql = "SELECT * FROM tb_user WHERE username='$username' AND password=md5('$password') LIMIT 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            if($row = $result->fetch_assoc()) {
+                if($row['is_active'] == 1){
+                    $_SESSION['username'] = $row['username'];
+                    echo "<h2>Login Success</h2>";
+                    header('Location: index.php');
+                }else{
+                    echo '<div class="alert alert-info alert-dismissible fade show" role="alert">
+                    <strong>Maaf!</strong> Akun anda belum aktif.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>';
+                }
+            }
+            // while($row = $result->fetch_assoc()) {
+            //     echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+            // }
+        } else {
             echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
             <strong>Maaf!</strong> Username atau Password Anda Salah.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -58,11 +80,6 @@
                                         id="Password"
                                         placeholder="Password">
                                 </div>
-                                <label class="mb-3">
-                                    <input type="checkbox" name="RememberMe">
-                                    Remember Me
-                                </label>
-                                <a href="#" class="float-end text-decoration-none">Reset Password</a>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-primary">Login</button>
                                 </div>
