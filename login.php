389172761d4187a11f1c02de6668520705a8ce62
ryanisml <ryanisml@gmail.com>
2024-04-29 08:38:39 +0800

    refactor login php script

--- login.php
+++ login.php
@@ -5,33 +5,6 @@ include 'database.php';
     if(isset($_SESSION['username'])){
         header('Location: index.php');
     }
-    if(isset($_POST['username']) && isset($_POST['password'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $sql = "SELECT * FROM tb_user WHERE username='$username' AND password=md5('$password') LIMIT 1";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            if($row = $result->fetch_assoc()) {
-                if($row['is_active'] == 1){
-                    $_SESSION['username'] = $row['username'];
-                    echo "<h2>Login Success</h2>";
-                    header('Location: index.php');
-                }else{
-                    echo '<div class="alert alert-info alert-dismissible fade show" role="alert">
-                    <strong>Maaf!</strong> Akun anda belum aktif.
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>';
-                }
-            }
-        } else {
-            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Maaf!</strong> Username atau Password Anda Salah.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
-        }
-    }
 ?>
 <!doctype html>
 <html lang="en">
@@ -57,6 +30,35 @@ include 'database.php';
                             <h3 class="text-center">Login Page</h3>
                         </div>
                         <div class="card-body">
+                            <?php 
+                            if(isset($_POST['username']) && isset($_POST['password'])){
+                                $username = $_POST['username'];
+                                $password = $_POST['password'];
+                        
+                                $sql = "SELECT * FROM tb_user WHERE username='$username' AND password=md5('$password') LIMIT 1";
+                                $result = $conn->query($sql);
+                        
+                                if ($result->num_rows > 0) {
+                                    if($row = $result->fetch_assoc()) {
+                                        if($row['is_active'] == 1){
+                                            $_SESSION['username'] = $row['username'];
+                                            echo "<h2>Login Success</h2>";
+                                            header('Location: index.php');
+                                        }else{
+                                            echo '<div class="alert alert-info alert-dismissible fade show" role="alert">
+                                            <strong>Maaf!</strong> Akun anda belum aktif.
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                          </div>';
+                                        }
+                                    }
+                                } else {
+                                    echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                    <strong>Maaf!</strong> Username atau Password Anda Salah.
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                  </div>';
+                                }
+                            }
+                            ?>
                             <form action="#" method="post">
                                 <div class="mb-3">
                                     <label for="username">Username</label>
